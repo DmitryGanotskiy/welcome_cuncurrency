fb36a15841d42c136c19f752d9a09b4fc83a828e
carlo8767 <carlo8767@gmail.com>
2026-01-24 17:47:19 +0100

    mutex_insertion

--- hello_c++/ThreadClass.cpp
+++ hello_c++/ThreadClass.cpp
@@ -3,39 +3,43 @@
 #include <thread>
 #include <utility>
 #include <string>
+#include <mutex>
+#include <unistd.h>
 
 
 
 class ThreadHello {
 
-    private: int number;
+    public: int number;
+    public: std::mutex createLock;
 
-    ThreadHello(int number){
+    ThreadHello(int value){
         // Arrow operator
-        this->number = number;
-
-    }
-
-    void helloLoop(){
-        std::string pippo = "String";
-        for(int i=0; i<5; i++){
-            std:: cout  << i << "pippo";
-            /// << is the "stream insertion operator" and >> is the stream extraction operator.
-            //They are meant repesent the direction the data is flowing. 
-            // Into the stream, or out of the stream. 
-            // Since "we" read from left to right, that allows for chaning.
-        }
+        number = value;
     }
 
     int getNumber(){
         return this->number;
     }
 
-     int main () {
-        ThreadHello t1 = ThreadHello(2);
+    void printResult(){
+        std::lock_guard  <std::mutex> lock(createLock);
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "Hello thread " << getpid() << "\n";;
+    }
 
- }
 
 };
 
+int main () {
+        ThreadHello t1 =  ThreadHello(2);
+        ThreadHello t2 = ThreadHello(3); // POINTER
+        std::thread ht1(&ThreadHello::printResult, &t1);
+        std::thread ht2(&ThreadHello::printResult, &t2);
+        t1.printResult();
+        t2.printResult();
+        ht1.join();
+        ht2.join();
+
+}
 
